db8045a670330e85d743a8e64f035ac5b943454b
ElyssonSantos <elyssongamer@gmail.com>
2026-07-19 22:28:37 -0300

    Update Watermark.tsx

--- src/src/Watermark.tsx
+++ src/src/Watermark.tsx
@@ -22,7 +22,7 @@ const Watermark = ({ className = "" }) => {
       onMouseLeave={(e) => (e.currentTarget.style.transform = "scale(1)")}
     >
       <img
-        src="/assets/watermark.png"
+        src="/assets/watermark2.png"
         alt="Site desenvolvido por Elysson Santos"
         style={{
           width: "170px",
